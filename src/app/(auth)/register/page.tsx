48d9ec74d82d354d117e5ea61e16d3a564a5da2f
RYAN NGUYEN <nguyenthanhtai112005@gmail.com>
2026-09-18 22:00:47 +0700

    feat(frontend): initialize react project using vite

--- src/app/(auth)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function RegisterPage() {
-    return (
-        <div>
-            <h1>Đăng ký</h1>
-        </div>
-    );
-}
---
+++ src/app/layout/App.tsx
@@ -0,0 +1,9 @@
+import "../../App.css";
+
+function App() {
+  return (
+    <div style={{fontSize: '1.2rem'}}>Điện Máy Lộc</div>
+  )
+}
+
+export default App
